daaa1c571bcb255b384521b11f58c50d1f14587c
Paloma González <palomabgz@gmail.com>
2025-05-23 19:37:02 -0300

    actualización

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Registro from './components/registro/registro'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
   return (
     <>
     <p>Portal de Mascotas, Polo IT 2025.</p>
+    <Registro/>
     </>
   )
 }
---
+++ src/components/Registro/Registro.jsx
@@ -0,0 +1,65 @@
+import './registro.css'
+const Registro = () => {
+    return (
+        <>
+        <div className='cabecera'>
+            <h2>Registro de mascota</h2>
+            <span class="paw-icon">🐾</span>
+        </div>
+        <div className="registro">
+            <form>
+                <div className="mb-3">
+                    <label htmlFor="fotoMascota" className="form-label">Foto de la mascota</label>
+                    <input className="form-control" type="file" id="fotoMascota"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="tipoMascota" className="form-label">Tipo de mascota</label>
+                    <select className="form-select" id="tipoMascota" required="">
+                    <option value="">Selecciona una opción</option>
+                    <option value="perro">Perro</option>
+                    <option value="gato">Gato</option>
+                    <option value="otro">Otro</option>
+                    </select>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="razaMascota" className="form-label">Raza</label>
+                    <input type="text" className="form-control" id="razaMascota" placeholder="Ej: Labrador o Mestizo"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="nombreMascota" className="form-label">Nombre</label>
+                    <input type="text" className="form-control" id="nombreMascota" placeholder="Ej: Max"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="edadMascota" className="form-label">Edad aproximada</label>
+                    <input type="number" className="form-control" id="edadMascota" placeholder="Ej: 2"/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="saludMascota" className="form-label">Estado de salud</label>
+                    <input type="text" className="form-control" id="saludMascota" placeholder="Ej: Vacunado, sano, necesita tratamiento..."/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="caracteristicasFisicas" class="form-label">Características físicas</label>
+                    <textarea class="form-control" id="caracteristicasFisicas" rows="2" placeholder="Color, tamaño, pelaje, etc."></textarea>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="historial" className="form-label">Historial de comportamiento/vida</label>
+                    <textarea class="form-control" id="historial" rows="3" placeholder="Ej: Amigable con niños, fue rescatado de la calle..."></textarea>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="requisitos" className="form-label">Requisitos para adopción</label>
+                    <textarea class="form-control" id="requisitos" rows="2" placeholder="Ej: Hogar con jardín, sin otras mascotas, etc."></textarea>
+                </div>
+                <div className="mb-4">
+                    <label htmlFor="infoAdicional" className="form-label">Información adicional (opcional)</label>
+                    <textarea class="form-control" id="infoAdicional" rows="2" placeholder="Comentarios adicionales..."></textarea>
+                </div>
+                <div className="d-grid">
+                    <button type="submit" className="boton-registro">Registrar Mascota</button>
+                </div>
+            </form>
+        </div>
+        </>
+    )
+}
+
+export default Registro;
